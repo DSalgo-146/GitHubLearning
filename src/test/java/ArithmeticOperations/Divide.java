46fce8328f6716562baa26a47c08bcdf1591cd83
jr <jayrengasamy@gmail.com>
2024-07-27 16:08:37 -0400

    Added sub and div functionalities

--- src/test/java/ArithmeticOperations/Divide.java
+++ src/test/java/ArithmeticOperations/Divide.java
@@ -1,5 +1,7 @@
 package ArithmeticOperations;
 
 public class Divide {
-
+    public static float divide(int num, int dem) {
+        return ((float)num / dem );
+    }
 }
--- src/test/java/ArithmeticOperations/Subtract.java
+++ src/test/java/ArithmeticOperations/Subtract.java
@@ -2,4 +2,7 @@ package ArithmeticOperations;
 
 public class Subtract {
 
+    public static subtract(int num1, int num2) {
+        return (num1 - num2 );
+    }
 }
